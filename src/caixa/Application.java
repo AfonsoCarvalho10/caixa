8bfb83a702eab6465231116fdfa56bf1adbd140d
Afonso Carvalho <210100471@esg.ipsantarem.pt>
2022-03-10 18:13:19 +0000

    Classe Caixa

--- src/caixa/Application.java
+++ src/caixa/Application.java
@@ -3,40 +3,10 @@ package caixa;
 public class Application {
 
 	public static void main(String[] args) {
-		int linhas = 9;
-		int colunas = 9;
-		
-		//TOPO
-		horizontal(colunas);
-		
-		
-		//LATERAL
-		vertical(linhas,colunas);
-		
-		//fim
-		horizontal(colunas);
+		Caixa cx = new Caixa(5,5);
+		cx.imprimir();
 		
 	}
-	private static void horizontal(int colunas) {
-		for (int i=0; i< 9; i++) {
-			System.out.print("0");
-		}
-		System.out.print("");
-	}
-	
-	private static void vertical(int linhas, int colunas) {
-		for (int i=0; i< 9; i++) {
-			System.out.print("0");
-			for(int x=0; x<colunas -2; x++)
-			{
-				System.out.print(" ");
-			}
-			System.out.print("0");
-			System.out.println("");
-		}
-		
-	}
-
-	}
+}
 
 
